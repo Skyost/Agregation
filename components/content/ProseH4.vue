0ec100869b1dc10882eb19745f5475b52c814418
Hugo Delaunay <Skyost@users.noreply.github.com>
2023-10-12 23:50:34 +0200

    Création de ProseH4.vue.

---
+++ components/content/ProseH4.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{ id?: string }>()
+</script>
+
+<template>
+  <h4 :id="id">
+    <slot />
+  </h4>
+</template>
+
+<style lang="scss" scoped>
+h3 {
+  counter-increment: headline-4;
+
+  &::before {
+    content: counter(headline-4, upper-alpha) '. ';
+  }
+}
+</style>
