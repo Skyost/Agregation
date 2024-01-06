456d3c34c99e7bdb5105c229a190bfa19fdf6e10
Hugo Delaunay <Skyost@users.noreply.github.com>
2024-01-06 15:50:58 +0100

    Amélioration du plugin Bootstrap.

--- plugins/bootstrap.client.ts
+++ plugins/bootstrap.client.ts
@@ -1,12 +1,8 @@
-import * as bootstrapCollapse from 'bootstrap/js/dist/collapse'
+import * as collapse from 'bootstrap/js/dist/collapse'
 
 /**
  * Bootstrap plugin for Nuxt.
  *
  * @param {NuxtApp} nuxtApp - The Nuxt app instance.
  */
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.provide('bootstrap', {
-    ...bootstrapCollapse
-  })
-})
+export default defineNuxtPlugin(nuxtApp => nuxtApp.provide('bootstrap', { collapse }))
