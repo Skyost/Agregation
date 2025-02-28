200fa483cb0a2db0dc97920201d517bd265989e8
Hugo Delaunay <Skyost@users.noreply.github.com>
2025-02-28 09:53:00 +0100

    Des améliorations sur la page d'accueil.

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
+import { siteMeta } from '~/site/meta'
+
 usePageHead({ title: 'Accueil' })
 
 const { data, status, error } = await useFetch<{ body: string }>('/_api/readme/')
+const onLinkClick = (event: MouseEvent) => {
+  if (event.target?.tagName === 'A' && event.target.href && (event.target.href.startsWith('/') || event.target.href.startsWith(siteMeta.url))) {
+    event.preventDefault()
+    useRouter().push(new URL(event.target.href).pathname)
+  }
+}
 </script>
 
 <template>
@@ -9,7 +17,10 @@ const { data, status, error } = await useFetch<{ body: string }>('/_api/readme/'
     <spinner />
   </div>
   <div v-else-if="data">
-    <div v-html="data.body" />
+    <div
+      @click="onLinkClick"
+      v-html="data.body"
+    />
   </div>
   <div v-else>
     <error-display :error="error" />
